8d22b05e1992e5b9c6cbb83e59f29c115b074c2a
dou <oziyano@sina.com>
2018-03-07 23:32:28 +0800

    update nio

--- work/src/main/java/com/ken/work/io/Client.java
+++ work/src/main/java/com/ken/work/io/Client.java
@@ -1,8 +1,6 @@
 package com.ken.work.io;
 
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.OutputStream;
+import java.io.*;
 import java.net.Socket;
 
 /**
@@ -17,23 +15,35 @@ public class Client {
         try {
 
             byte[] buffer = new byte[50];
-            while (true) {
-                System.out.println("write");
-                OutputStream out = socket.getOutputStream();
-                out.write("hello".getBytes("UTF-8"));
 
+            System.out.println("write");
+            OutputStream out = socket.getOutputStream();
+            out.write("hello".getBytes("UTF-8"));
+            out.flush();
 
-                System.out.println("read");
-                InputStream in = socket.getInputStream();
-                int count;
-                while ((count = in.read(buffer)) > 0) {
+//
+//            InputStream in = socket.getInputStream();
+//            int count;
+//            while ((count = in.read(buffer)) != -1) {
+//                String s = new String(buffer, 0, count);
+//                System.out.println(s);
+//            }
+//
 
-                    String s = new String(buffer, 0, count);
-                    System.out.println(s);
-                }
+            InputStream in=socket.getInputStream();
+            BufferedReader br=new BufferedReader(new InputStreamReader(in));
+            //3.利用流按照一定的操作，对socket进行读写操作
+            String replyInfo=null;
+            while(!((replyInfo=br.readLine())==null)){
+                System.out.println("接收服务器的数据信息："+replyInfo);
             }
-        } finally {
+
+
+            out.close();
+            in.close();
             socket.close();
+        } finally {
+//            socket.close();
         }
 
     }
--- work/src/main/java/com/ken/work/io/NotBlockedIO.java
+++ work/src/main/java/com/ken/work/io/NotBlockedIO.java
@@ -57,12 +57,14 @@ public class NotBlockedIO {
         } else if (currentKey.isReadable()) {
 
             SocketChannel socketChannel = (SocketChannel) currentKey.channel();
+//            handleRead(currentKey);
             handleRead(socketChannel);
-            socketChannel.register(selector, SelectionKey.OP_WRITE);
+//            socketChannel.register(selector, SelectionKey.OP_WRITE);
 
         } else if (currentKey.isWritable()) {
-            SocketChannel socketChannel = (SocketChannel) currentKey.channel();
-            handleWrite(socketChannel);
+            System.out.println("writable");
+//            SocketChannel socketChannel = (SocketChannel) currentKey.channel();
+//            handleWrite(socketChannel);
         }
 
     }
@@ -78,28 +80,72 @@ public class NotBlockedIO {
     private void handleRead(SocketChannel socketChannel)  {
 
         ByteBuffer readBuffer = ByteBuffer.allocate(2);
-        ByteBuffer writeBuffer = ByteBuffer.allocate(20);
 
         int count;
         try {
-            while ((count = socketChannel.read(readBuffer)) > 0) {
-                String s = new String(readBuffer.array(), "UTF-8");
-                System.out.println(s);
+            System.out.println("--read: ");
+            while (true) {
+
+                count = socketChannel.read(readBuffer);
+                if (count > 0) {
+
+                    String s = new String(readBuffer.array(), 0 ,count, "UTF-8");
+                    System.out.println(s);
+
+                    readBuffer.flip();
+                } else {
+                    System.out.println("count = " + count);
+                    break;
+                }
             }
 
-            writeBuffer.put("server".getBytes("UTF-8"));
-            socketChannel.write(writeBuffer);
+            System.out.println("--write: ");
+             ByteBuffer writeBuffer = ByteBuffer.allocate(2);
+            System.out.println(writeBuffer.remaining());
+            byte[] data = "server".getBytes("UTF-8");
 
+            for(int i = 0; i < data.length;) {
+
+                int limit = writeBuffer.limit();
+                limit = (data.length - i) > limit ? limit : data.length - i;
+
+                writeBuffer.clear();
+                System.out.println("i=" + i + " limit=" + limit + " remain=" + writeBuffer.remaining());
+                writeBuffer.put(data, i, limit);
+                System.out.println("i=" + i + " limit=" + limit + " remain=" + writeBuffer.remaining());
+                writeBuffer.flip();
+                socketChannel.write(writeBuffer);
+
+                i = i + limit;
+            }
+
+            System.out.println("exist");
+
+
+            socketChannel.close();
         } catch (IOException ioe) {
             ioe.printStackTrace();
-            try {
-                socketChannel.close();
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
         }
     }
 
+
+    public void handleRead(SelectionKey key) throws IOException {
+        ByteBuffer byteBuffer = ByteBuffer.allocate(512);
+        SocketChannel socketChannel = (SocketChannel)key.channel();
+        while(true) {
+            int readBytes = socketChannel.read(byteBuffer);
+            if(readBytes>0) {
+                String s = new String(byteBuffer.array(), 0, readBytes);
+                System.out.println("Server: data = " + s);
+//                log.info("Server: readBytes = " + readBytes);
+//                log.info("Server: data = " + new String(byteBuffer.array(), 0, readBytes));
+                byteBuffer.flip();
+                socketChannel.write(byteBuffer);
+                break;
+            }
+        }
+        socketChannel.close();
+    }
 
     public static void main(String[] args) throws IOException {
         NotBlockedIO notBlockedIO = new NotBlockedIO();
